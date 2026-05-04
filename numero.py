041c00c9a0e40f51d25b9366e162181b787c0625
Talo <talo.italo.ng@gmail.com>
2026-05-04 17:38:24 -0500

    Correcion constructor Numero

--- numero.py
+++ numero.py
@@ -1,9 +1,9 @@
 class Numero:
-    def _init_(self, numero: int):
+    def __init__(self, numero: int):
         self.numero = numero
     
     def validarPrimo(self) -> bool:
-        if self.numero == 1 or self.numero == 0:
+        if self.numero < 2:
             return False
         if self.numero == 2:
             return True
@@ -11,5 +11,4 @@ class Numero:
             for i in range(2, self.numero):
                 if self.numero % i == 0:
                     return False
-                elif self.numero % i != 0 and i == self.numero - 1:
-                    return True
+            return True
