a72749502d789dc7e86cd7fe2332714803b684ca
Talo <talo.italo.ng@gmail.com>
2026-05-04 16:46:49 -0500

    Nuevo metodo validar Primo

--- numero.py
+++ numero.py
@@ -3,7 +3,13 @@ class Numero:
         self.numero = numero
     
     def validarPrimo(self) -> bool:
-        for i in range(2, self.numero):
-            if self.numero % i == 0:
-                return False
-        return True
+        if self.numero == 1 or self.numero == 0:
+            return False
+        if self.numero == 2:
+            return True
+        elif self.numero > 2:
+            for i in range(2, self.numero):
+                if self.numero % i == 0:
+                    return False
+                elif self.numero % i != 0 and i == self.numero - 1:
+                    return True
