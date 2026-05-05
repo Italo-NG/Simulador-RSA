9657e67ea8d76280429d2d3cce865afa4b08217d
Talo <talo.italo.ng@gmail.com>
2026-05-05 00:45:44 -0500

    Implementacion post Validar primos

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from numero import Numero
 
-numero1 = 2
-numero2 = 5
+app = FastAPI()
 
-if Numero(numero1).validarPrimo() and Numero(numero2).validarPrimo():
-    print(f"{numero1} y {numero2} son primos")
-else:
-    print(f"{numero1} y {numero2} no son primos")
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"],)
+
+class NumerosRequest(BaseModel):
+    primerNumero: int
+    segundoNumero: int
+
+@app.post("/api/validar-primos")
+def validar_primos(datos: NumerosRequest):
+    primerNumero = datos.primerNumero
+    segundoNumero = datos.segundoNumero
+
+    primerNumeroEsPrimo = Numero(primerNumero).validarPrimo()
+    segundoNumeroEsPrimo = Numero(segundoNumero).validarPrimo()
+
+    if primerNumeroEsPrimo and segundoNumeroEsPrimo:
+        mensaje = f"{primerNumero} y {segundoNumero} son números primos."
+    else:
+        mensaje = f"{primerNumero} y {segundoNumero} no cumplen la condición de ser ambos primos."
+
+    return {
+        "primer_numero": primerNumero,
+        "segundo_numero": segundoNumero,
+        "primer_numero_es_primo": primerNumeroEsPrimo,
+        "segundo_numero_es_primo": segundoNumeroEsPrimo,
+        "ambos_son_primos": primerNumeroEsPrimo and segundoNumeroEsPrimo,
+        "mensaje": mensaje
+    }
